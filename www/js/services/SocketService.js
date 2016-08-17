f9f5ef98209b8e9c1a16b92b7d07fa4e1c0b9449
Lucas Barbula <lucas.barbula@gmail.com>
2016-08-17 11:04:51 -0600

    local changes

--- www/js/services/SocketService.js
+++ www/js/services/SocketService.js
@@ -5,8 +5,8 @@
 
 	function SocketService(socketFactory){
 		return socketFactory({
-			
-			ioSocket: io.connect('http://localhost:3000')
+
+			ioSocket: io.connect('http://10.6.82.246:3000')
 
 		});
 	}
