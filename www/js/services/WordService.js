fce1a5ad72461ab96e447e50589e758a3911ce5c
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-19 10:47:49 -0600

    fix definition trim

--- www/js/services/WordService.js
+++ www/js/services/WordService.js
@@ -4,11 +4,11 @@ angular.module('starter')
       getWord: function() {
         return $https.get('https://q3-socket-server.herokuapp.com/word').then(function(word) {
           var def = word.data.meaning;
-          var re = /\).*\(/;
+          var re = /\).*\(*/;
           var found = def.match(re)[0]
           found = found.replace(/\)|\(/g, '').trim();
-          found = found.split(';')[0];
-          word.data.meaning = found;
+          found = found.split(';');
+          word.data.meaning = found[0];
           return $q.resolve(word);
         })
       }
